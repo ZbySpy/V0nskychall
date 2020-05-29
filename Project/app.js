ab25ecbe1d6d9dce1efc7b5b3476dcdddfa6148a
ZbySpy <64301430+ZbySpy@users.noreply.github.com>
2020-05-30 00:08:38 +0200

    v2

--- Project/app.js
+++ Project/app.js
@@ -11,11 +11,11 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/views'));
 
 const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123456'));
 const session = driver.session();
